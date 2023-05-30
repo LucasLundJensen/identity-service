a29d619bfed327694965a3d8365eb63bc5228731
Lucas Lund Jensen <lucas.lund@live.dk>
2023-05-30 16:21:35 +0200

    feat(wip): setup ejs for templating login & authorize for OIDC OIDC configuration now also changes interactions request path to use /oidc/ as prefix temporarily enabled CORS for everything

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import fastify from "fastify";
 import fastifyMiddie from "@fastify/middie";
+import fasitfyView from '@fastify/view';
+import ejs from 'ejs';
 import { envToLogger } from "./logging.js";
 import config from "./config.js";
 import oidc from "./oidc.js";
 // import { addUser, getUserById } from "./services/user.service";
 import redisClient from "./redis.js";
+
+import AuthRoutes from "./routes/auth.routes.js";
 
 export const server = fastify({ logger: envToLogger(config.ENV) ?? true });
 
@@ -13,6 +17,13 @@ const start = async () => {
 		await redisClient.connect();
 
 		await server.register(fastifyMiddie);
+		await server.register(fasitfyView, {
+			engine: {
+				ejs
+			}
+		});
+
+		await server.register(AuthRoutes)
 
 		server.use("/oidc", oidc.callback());
 
--- src/oidc.ts
+++ src/oidc.ts
@@ -1,9 +1,23 @@
-import Provider from "oidc-provider";
+import Provider, { Configuration } from "oidc-provider";
 import config from "./config.js";
 import clients from "../config/clients.json" assert { type: "json" };
 
-const configuration = {
-	clients,
+/**
+ * Rewrite the interactions.url function to reflect the path we setup for fastify to use.
+ */
+
+const configuration: Configuration = {
+	clients: [
+		...clients as any,
+	],
+	interactions: {
+		url(ctx, interaction) {
+			return `oidc/interaction/${interaction.uid}`
+		},
+	},
+	clientBasedCORS(ctx, origin, client) {
+		return true;
+	},
 };
 
 const oidc = new Provider(
